10ba8a586cae50033643961a07196ffc3d8b21da
Gaspar Tartari <gaspartartari@gmail.com>
2023-10-26 17:35:40 -0300

    Loop funcion until valid input

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,35 +18,45 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         DateTimeFormatter sdf = DateTimeFormatter.ofPattern("dd/MM/yyyy"); 
-        
-        try{
-            System.out.println("Enter contract data: ");
-            System.out.print("Number: ");
-            Integer numberC = sc.nextInt();
-            sc.nextLine();
-            System.out.print("Date (dd/mm/yyyy): ");
-            LocalDate date = LocalDate.parse(sc.next(), sdf);
-            System.out.print("Contract value: ");
-            Double value = sc.nextDouble();
-            sc.nextLine();
-            System.out.print("Enter number of installments: ");
-            Integer numberI = sc.nextInt();
-            sc.nextLine();
-            Contract contract = new Contract(numberC, date, value);
-            ContractService contractService = new ContractService(new PaypalService());
-            
-            contractService.processContract(contract, numberI);
-            System.out.println("Installments:");
-            for (Installment i : contract.getInstallments()){
-                System.out.println(i);
-            }
-        }
-        catch (InputMismatchException e ){
-            System.out.println("Incorrect format. Please enter a valid value");
-        }
-        catch (DateTimeParseException e){
-            System.out.println("Incorrect format. Please enter date format (dd/MM/yyyy)");
-        }
+        int menu = 0;
+        while(menu == 0){
+                try{    
+                    System.out.println("Enter contract data: ");
+                    System.out.print("Number: ");
+                    Integer numberC = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Date (dd/mm/yyyy): ");
+                    LocalDate date = LocalDate.parse(sc.next(), sdf);
+                    System.out.print("Contract value: ");
+                    Double value = sc.nextDouble();
+                    sc.nextLine();
+                    System.out.print("Enter number of installments: ");
+                    Integer numberI = sc.nextInt();
+                    sc.nextLine();
+                    Contract contract = new Contract(numberC, date, value);
+                    ContractService contractService = new ContractService(new PaypalService());
+                    
+                    contractService.processContract(contract, numberI);
+                    System.out.println("Installments:");
+                    for (Installment i : contract.getInstallments()){
+                        System.out.println(i);
+                    }
+                    menu =1;
+                        
+                    }
+                    catch (InputMismatchException e ){
+                        System.out.println("Incorrect format. Please enter a valid value");
+                        sc.nextLine();
+                        menu =0;
+                        
+                    }
+                    catch (DateTimeParseException e){
+                        System.out.println("Incorrect format. Please enter date format (dd/MM/yyyy)");
+                        sc.nextLine();
+                        menu=0;
+                        
+                }
+        }    
         sc.close();
     }
 }
